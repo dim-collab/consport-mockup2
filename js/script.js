100f8666092e86a59d372ffcd7fb96d81286ffbd
dim-collab <dim75789@gmail.com>
2026-04-20 15:29:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,3 +53,26 @@ const nav = document.querySelector(".nav-links");
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+// ACTIVE MENU ON SCROLL
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(a => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") === "#" + current) {
+      a.classList.add("active");
+    }
+  });
+});
